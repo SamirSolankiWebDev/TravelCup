ee8bac3d794ac4397973832c6c249313b7e274a9
Samir Ashok Solanki <W0785689@myscc.ca>
2022-10-17 04:55:40 -0400

    Commit Added Parameter First PArt of fourth

---
+++ TravelCup/Models/TravelCup.cs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TravelCup.Models
+{
+    public class TravelCup
+    {
+        public int Id { get; set; }
+        public string CupsTitle { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ManufactureDate { get; set; }
+        public string Shape { get; set; }
+        public string Colour { get; set; }
+        public string Size { get; set; }
+        public decimal Price { get; set; }
+    }
+}
