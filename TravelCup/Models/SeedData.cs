cc9d5afb121d2f314c33641b2c628230337469af
Samir Ashok Solanki <W0785689@myscc.ca>
2022-10-17 05:56:52 -0400

    Commit Seed Data

---
+++ TravelCup/Models/SeedData.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TravelCup.Data;
+using System;
+using System.Linq;
+
+namespace TravelCup.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcTravelCupContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcTravelCupContext>>()))
+            {
+                // Look for any movies.
+                if (context.TravelCupClass.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.TravelCupClass.AddRange(
+                    new TravelCupClass
+                    {
+                        CupsTitle = "WorldCup",
+                        ManufactureDate = DateTime.Parse("2000-26-12"),
+                        Shape = "Circle",
+                        Colour = "Red",
+                        Size = "Small",
+                        Price = 1200.10M
+                    },
+
+                   new TravelCupClass
+                   {
+                       CupsTitle = "WorldCup",
+                       ManufactureDate = DateTime.Parse("2000-26-12"),
+                       Shape = "Circle",
+                       Colour = "Red",
+                       Size = "Small",
+                       Price = 1200.10M
+                   },
+
+                   new TravelCupClass
+                   {
+                       CupsTitle = "WorldCup",
+                       ManufactureDate = DateTime.Parse("2000-26-12"),
+                       Shape = "Circle",
+                       Colour = "Red",
+                       Size = "Small",
+                       Price = 1200.10M
+                   },
+
+                    new TravelCupClass
+                    {
+                        CupsTitle = "WorldCup",
+                        ManufactureDate = DateTime.Parse("2000-26-12"),
+                        Shape = "Circle",
+                        Colour = "Red",
+                        Size = "Small",
+                        Price = 1200.10M
+                    },
+                     new TravelCupClass
+                     {
+                         CupsTitle = "WorldCup",
+                         ManufactureDate = DateTime.Parse("2000-26-12"),
+                         Shape = "Circle",
+                         Colour = "Red",
+                         Size = "Small",
+                         Price = 1200.10M
+                     },
+                      new TravelCupClass
+                      {
+                          CupsTitle = "WorldCup",
+                          ManufactureDate = DateTime.Parse("2000-26-12"),
+                          Shape = "Circle",
+                          Colour = "Red",
+                          Size = "Small",
+                          Price = 1200.10M
+                      },
+                       new TravelCupClass
+                       {
+                           CupsTitle = "WorldCup",
+                           ManufactureDate = DateTime.Parse("2000-26-12"),
+                           Shape = "Circle",
+                           Colour = "Red",
+                           Size = "Small",
+                           Price = 1200.10M
+                       },
+                        new TravelCupClass
+                        {
+                            CupsTitle = "WorldCup",
+                            ManufactureDate = DateTime.Parse("2000-26-12"),
+                            Shape = "Circle",
+                            Colour = "Red",
+                            Size = "Small",
+                            Price = 1200.10M
+                        },
+                         new TravelCupClass
+                         {
+                             CupsTitle = "WorldCup",
+                             ManufactureDate = DateTime.Parse("2000-26-12"),
+                             Shape = "Circle",
+                             Colour = "Red",
+                             Size = "Small",
+                             Price = 1200.10M
+                         },
+                          new TravelCupClass
+                          {
+                              CupsTitle = "WorldCup",
+                              ManufactureDate = DateTime.Parse("2000-26-12"),
+                              Shape = "Circle",
+                              Colour = "Red",
+                              Size = "Small",
+                              Price = 1200.10M
+                          }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
