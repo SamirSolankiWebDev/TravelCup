41a3e8a54b44bd93dfa6e244a9a78e8245312e7e
Samir Ashok Solanki <W0785689@myscc.ca>
2022-10-17 06:18:45 -0400

    Part 6 DOne

--- TravelCup/Models/TravelCupClass.cs
+++ TravelCup/Models/TravelCupClass.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TravelCup.Models
 {
@@ -11,11 +12,15 @@ namespace TravelCup.Models
         public int Id { get; set; }
         public string CupsTitle { get; set; }
 
+        [Display(Name = "ManufactureDate")]
         [DataType(DataType.Date)]
         public DateTime ManufactureDate { get; set; }
         public string Shape { get; set; }
         public string Colour { get; set; }
         public string Size { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
     }
 }
