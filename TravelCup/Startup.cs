3e86cbc117d42d9afa9a107e1882bb119923ad6b
Samir Ashok Solanki <W0785689@myscc.ca>
2022-10-17 05:17:42 -0400

    Examin Data connection String

--- TravelCup/Startup.cs
+++ TravelCup/Startup.cs
@@ -1,9 +1,10 @@
+using TravelCup.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,7 +12,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using TravelCup.Data;
 
 namespace TravelCup
 {
