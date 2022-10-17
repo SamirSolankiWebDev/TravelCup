032cb21340d06f3db4a46beeb83bd6b2636f304e
Samir Ashok Solanki <W0785689@myscc.ca>
2022-10-17 04:43:04 -0400

    Commit Till Second Part Privacy

--- TravelCup/Controllers/HelloWorldController.cs
+++ TravelCup/Controllers/HelloWorldController.cs
@@ -9,18 +9,18 @@ namespace TravelCup.Controllers
 {
     public class HelloWorldController : Controller
     {
-        /* public IActionResult Index()
-         {
-             return View();
-         }*/
+        public IActionResult Index()
+        {
+            return View();
+        }
 
         // 
         // GET: /HelloWorld/
 
-        public string Index()
-        {
-            return "This is my default action...";
-        }
+        /* public string Index()
+         {
+             return "This is my default action...";
+         }*/
 
         // 
         // GET: /HelloWorld/Welcome/ 
